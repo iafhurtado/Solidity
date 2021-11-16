7b7386d8e8d5bdc703426013f9f82b580e68a5b1
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2021-11-16 14:29:36 +0100

    Verify task on ropsten

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,20 @@
 require("@nomiclabs/hardhat-waffle");
+require("solidity-coverage");
+require("@nomiclabs/hardhat-etherscan");
+
+task("deploy-mainnet", "Deploys contract on a provided network")
+    .addParam("privateKey", " Please provide the private key")
+    .setAction(async ({privateKey}) => {
+        const deployElectionContract = require("./scripts/deploy-with-param");
+        await deployElectionContract(privateKey);
+    });
+
+subtask("print", "Prints a message")
+    .addParam("message", "The message to print")
+    .setAction(async (taskArgs) => {
+        await hre.run('print', { message: "Done!" })
+        console.log(taskArgs.message);
+    });
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -17,5 +33,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.0",
+  networks: {
+    ropsten: {
+      url: "https://ropsten.infura.io/v3/40c2813049e44ec79cb4d7e0d18de173",
+    }
+  },
+	
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: "CHIRAADNUI814XIT9ST36R63UFNBNDKBDY"
+  }
 };
